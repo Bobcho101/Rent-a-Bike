e6cb5fabe912cb91413f0fc34f3e049aef1d0df1
Bobcho101 <bgbobi878@gmail.com>
2024-12-21 13:24:57 +0200

    setupped firebase
    settuped firebase and added some view js fles

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "../../node_modules/firebase/firebase-app.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD6dw4cW8aMx58_zjBB0zt1QaBAsO5Orxw",
+    authDomain: "rent-a-bike-try.firebaseapp.com",
+    databaseURL: "https://rent-a-bike-try-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "rent-a-bike-try",
+    storageBucket: "rent-a-bike-try.firebasestorage.app",
+    messagingSenderId: "1091736204113",
+    appId: "1:1091736204113:web:c37128408e774576495970"
+};
+
+
+const app = initializeApp(firebaseConfig);
